f310a3a66df8c8c371d6653468c690cb2a8b12f4
Craig Warren <craig@craigwarren.co.uk>
2019-12-02 08:31:28 +0000

    tidy up day 2 puzzel 2

--- day2/puzzel2.py
+++ day2/puzzel2.py
@@ -3,8 +3,8 @@ from day2.computer import calculate
 
 target = 19690720
 
-for noun in range (0, 100):
-    for verb in range (0, 100):
-        result = calculate(noun = noun, verb = verb)
+for noun in range(0, 100):
+    for verb in range(0, 100):
+        result = calculate(noun=noun, verb=verb)
         if result == target:
             print("answer is {}".format(100 * noun + verb))
